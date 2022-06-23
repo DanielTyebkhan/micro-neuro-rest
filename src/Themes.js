def944db27a6c061fb5b379fb4ee612b54816210
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-06-22 17:28:01 -0700

    added starting template

---
+++ src/Themes.js
@@ -0,0 +1,19 @@
+export const darkTheme = {
+  name: 'Dark',
+  body: '#121212',
+  text: '#FFFFFF',
+  button: {
+    body: '#1DA1F2',
+    text: '#FFFFFF',
+  },
+};
+
+export const lightTheme = {
+  name: 'Light',
+  body: '#FFFFFF',
+  text: '#000000',
+  button: {
+    body: '#121212',
+    text: '#FFFFFF',
+  },
+};
